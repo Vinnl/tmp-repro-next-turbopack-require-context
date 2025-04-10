de017c9ebccf7dd60864bdd8105bc252354418a0
Vincent <git@vincenttunru.com>
2025-04-10 12:44:28 +0200

    Reproduce bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
+  const translationsContext = (require as any).context(
+    "./grandparent",
+    true,
+    /\.txt/,
+  );
+  // In Webpack, logs:
+  //     [ './parent/file.txt' ]
+  // In Turbopack, logs:
+  //     [ './file.txt' ]
+  console.log(translationsContext.keys());
   return null;
 }
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  turbopack: {
+    rules: {
+      "*.txt": {
+        loaders: ["raw-loader"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
